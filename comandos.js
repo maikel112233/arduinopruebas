aa03263537603bba46359b972afff466620ccba5
maikel112233 <maikel112233@gmail.com>
2017-01-27 12:26:56 +0100

    tres

--- comandos.js
+++ comandos.js
@@ -2,7 +2,8 @@
 	{
 		"ctrl-Q":"reinicia la consola",
 		"ctrl-S+stty-ixon":"evitar que se bloquee y introduzca stty-ixon ",
-			"ok"
+		"ctrl-A":"desplaza el cursor al principio",
+		"ctrl-E":"desplaza el cursor al final",
 			
 	}
 
